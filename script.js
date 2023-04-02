7bbe1741a35394fb307390cc8cf4837f5f8449be
Inzamamul Hasan <107973468+InzamamulHasan@users.noreply.github.com>
2023-04-03 00:24:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 
 function myfun() {
 	let inps=document.getElementById("evaluatedText").value;
+	inps=inps.trim();
 
 	let s=inps.split(" ");
-	document.getElementById("wordCount").innerHTML=s.length;
+	let size1=s.length;
+	/*if(inps[inps.length-1]=" "){
+		size1=size1-1;
+	}*/
+	document.getElementById("wordCount").innerHTML=size1;
 	
 } 
